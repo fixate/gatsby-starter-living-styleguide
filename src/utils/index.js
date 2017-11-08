fdd9908f5d126eaeacf33951af287e91c67bd736
Larry Botha <larry@fixate.it>
2017-11-08 20:40:11 +0200

    add text helper

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,17 @@
 export const getYamlNode = (data, name) =>
   data.allDataYaml.edges.find(e => e.node[name]).node[name];
+
+export const splitCamelCaseString = (joiner = '-') => str =>
+  str
+    .split('')
+    .reduce((acc, c) => {
+      const isUpperCase = /[A-Z]/.test(c);
+      const newAcc = isUpperCase
+        ? acc.concat([c.toLowerCase()])
+        : acc
+            .slice(0, -1)
+            .concat((acc.slice(-1)[0] ? acc.slice(-1)[0] : []).concat(c));
+
+      return newAcc;
+    }, [])
+    .join(joiner);
