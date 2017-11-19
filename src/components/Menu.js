ab09e0b370f69ea8a6546738a22a6fd52895c5c9
Larry Botha <larry@fixate.it>
2017-11-19 14:09:40 +0200

    prettier

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -16,8 +16,9 @@ const reduceCats = (acc, cat) => {
   let newAcc = hasCat ? acc : acc.concat({name: cat.name, subCats: []});
   newAcc = cat.subCat
     ? newAcc.map(c => {
-        const alreadyHasChild = (c.subCats || [])
-          .some(subCat => subCat.name === cat.subCat.name);
+        const alreadyHasChild = (c.subCats || []).some(
+          subCat => subCat.name === cat.subCat.name
+        );
         c.subCats =
           c.name === cat.name && !alreadyHasChild
             ? c.subCats.concat(cat.subCat).sort((a, b) => a.name > b.name)
@@ -48,65 +49,60 @@ const Menu = ({pages}) => {
           p => p.relativeDirectory.split('/').length === 1
         );
 
-        return isIndex
-          ? <div style={{marginBottom: '1em'}} key={cat.name}>
-              <Link to="/">home</Link>
-            </div>
-          : <div key={cat.name}>
-              {cat.name
-                ? <h4 style={{marginBottom: 0}}>
-                    {cat.name.replace('-', ' ')}
-                  </h4>
-                : null}
+        return isIndex ? (
+          <div style={{marginBottom: '1em'}} key={cat.name}>
+            <Link to="/">home</Link>
+          </div>
+        ) : (
+          <div key={cat.name}>
+            {cat.name ? (
+              <h4 style={{marginBottom: 0}}>{cat.name.replace('-', ' ')}</h4>
+            ) : null}
 
-              {rootCatPages.length
-                ? <div
-                    key={cat.name}
-                    style={{marginBottom: '1em', marginLeft: '1em'}}>
-                    {rootCatPages.map(cp => {
-                      const name = cp.name === 'index' ? '' : cp.name;
+            {rootCatPages.length ? (
+              <div
+                key={cat.name}
+                style={{marginBottom: '1em', marginLeft: '1em'}}>
+                {rootCatPages.map(cp => {
+                  const name = cp.name === 'index' ? '' : cp.name;
 
-                      return (
+                  return (
+                    <div key={cp.name}>
+                      <Link to={`${cp.relativeDirectory}/${name}`}>
+                        {cp.name.replace('-', ' ')}
+                      </Link>
+                    </div>
+                  );
+                })}
+              </div>
+            ) : null}
+
+            {cat.subCats.length
+              ? cat.subCats.map(subCat => {
+                  const subCatPages = catPages.filter(
+                    p =>
+                      p.relativeDirectory === [cat.name, subCat.name].join('/')
+                  );
+
+                  return (
+                    <div
+                      key={subCat.name}
+                      style={{marginBottom: '1em', marginLeft: '1em'}}>
+                      <h4 style={{marginBottom: 0}}>{subCat.name}</h4>
+
+                      {subCatPages.map((cp, i) => (
                         <div key={cp.name}>
-                          <Link to={`${cp.relativeDirectory}/${name}`}>
-                            {cp.name.replace('-', ' ')}
+                          <Link to={`${cp.relativeDirectory}/${cp.name}`}>
+                            {cp.name.replace(/-/g, ' ')}
                           </Link>
                         </div>
-                      );
-                    })}
-                  </div>
-                : null}
-
-              {cat.subCats.length
-                ? cat.subCats.map(subCat => {
-                    const subCatPages = catPages.filter(
-                      p =>
-                        p.relativeDirectory ===
-                        [cat.name, subCat.name].join('/')
-                    );
-
-                    return (
-                      <div
-                        key={subCat.name}
-                        style={{marginBottom: '1em', marginLeft: '1em'}}>
-                        <h4 style={{marginBottom: 0}}>
-                          {subCat.name}
-                        </h4>
-
-                        {subCatPages.map((cp, i) =>
-                          <div key={cp.name}>
-                            <Link
-                              to={`${cp.relativeDirectory}/${name}${cp.name}`}>
-                              {cp.name.replace(/-/g, ' ')}
-                            </Link>
-                          </div>
-                        )}
-                      </div>
-                    );
-                  })
-                : null}
-            </div>;
-			  <Link to={`${cp.relativeDirectory}/${cp.name}`}>
+                      ))}
+                    </div>
+                  );
+                })
+              : null}
+          </div>
+        );
       })}
     </nav>
   );
