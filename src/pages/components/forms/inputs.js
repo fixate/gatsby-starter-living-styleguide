1eccceada31c114c4491080b971df5c4a318823a
Larry Botha <larry@fixate.it>
2017-11-21 14:05:58 +0200

    fix react warnings

--- src/pages/components/forms/inputs.js
+++ src/pages/components/forms/inputs.js
@@ -9,18 +9,18 @@ const Inputs = () => {
         <div className="field">
           <label htmlFor="">label</label>
 
-          <input type="text" value="value text" />
+          <input type="text" defaultValue="value text" />
         </div>
 
         <div className="field">
           <label htmlFor="">label</label>
 
-          <input type="text" value="" placeholder="placeholder text" />
+          <input type="text" defaultValue="" placeholder="placeholder text" />
         </div>
 
         <div className="field">
           <label htmlFor="">label</label>
-          <input type="text" value="" placeholder="placeholder text" />
+          <input type="text" defaultValue="" placeholder="placeholder text" />
           <span>with suffix</span>
         </div>
       </form>
@@ -29,12 +29,12 @@ const Inputs = () => {
       <form>
         <div className="field">
           <label htmlFor="">label</label>
-          <input type="password" value="password" />
+          <input type="password" defaultValue="password" />
         </div>
 
         <div className="field">
           <label htmlFor="">label</label>
-          <input type="password" value="" placeholder="password" />
+          <input type="password" defaultValue="" placeholder="password" />
         </div>
       </form>
 
@@ -43,9 +43,13 @@ const Inputs = () => {
       <form>
         <div className="field">
           <label htmlFor="">label</label>
-          <textarea id="" name="" cols="30" rows="10">
-            value text
-          </textarea>
+          <textarea
+            id=""
+            name=""
+            cols="30"
+            rows="10"
+            defaultValue="value text"
+          />
         </div>
 
         <div className="field">
@@ -56,6 +60,7 @@ const Inputs = () => {
             cols="30"
             rows="10"
             placeholder="placeholder text"
+            defaultValue=""
           />
         </div>
       </form>
