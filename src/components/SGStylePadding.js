5abbb597fdde7a5724575874518f46da95862b21
Larry Botha <larry@fixate.it>
2017-12-30 19:19:28 +0200

    rewrite withStyledPadding HOC as render prop

---
+++ src/components/SGStylePadding.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+const StylePadding = ({render}) => {
+  const style = {
+    backgroundColor: '#c4dcb7',
+  };
+
+  return render({style});
+};
+
+export default StylePadding;
